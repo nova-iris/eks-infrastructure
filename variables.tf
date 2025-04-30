889300d62c694f9c904ee8546e5310a028398fba
Trong Truong <trongtruong2509@gmail.com>
2025-04-30 14:38:03 +0700

    fix node group disire

--- variables.tf
+++ variables.tf
@@ -53,10 +53,11 @@ variable "eks_managed_node_groups" {
     capacity_type  = string
     node_labels    = map(string)
   })
+
   default = {
     min_size       = 1
-    max_size       = 3
-    desired_size   = 2
+    max_size       = 5
+    desired_size   = 3
     instance_types = ["t3.medium"]
     capacity_type  = "ON_DEMAND"
     node_labels = {
